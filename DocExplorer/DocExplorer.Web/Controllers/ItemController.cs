2c9ca30f64943c7c636eb5f6c3474424b61ee125
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2013-08-03 17:55:00 +0700

    browse and create page

--- DocExplorer/DocExplorer.Web/Controllers/ItemController.cs
+++ DocExplorer/DocExplorer.Web/Controllers/ItemController.cs
@@ -8,8 +8,18 @@ using DocExplorer.Models;
 namespace DocExplorer.Web.Controllers {
   public class ItemController : Controller {
 
+    private DocContext db = new DocContext();
+    protected override void Dispose(bool disposing) {
+      db.Dispose();
+      base.Dispose(disposing);
+    }
+
     public ActionResult Index() {
-      return View();
+      var q = from i in db.Items
+              orderby i.DateCreated ascending
+              select i;
+      var items = q.ToList();
+      return View(items);
     }
 
     public ActionResult Create() {
